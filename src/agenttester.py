b52f5a6b9fd12f2a3abf6a915cd417c4af58af36
Nuno Das Neves <ndasneves@gmail.com>
2019-11-07 17:41:52 -0800

    track avg and longest move time

--- src/agenttester.py
+++ src/agenttester.py
@@ -14,21 +14,34 @@ def main():
     parser = argparse.ArgumentParser(description="Run batches of games to test agents")
     parser.add_argument('-n', '--num-games', type=int, default=1, dest='num_games', help='number of games')
     args = parser.parse_args()
-    agent_scores = [0, 0]
     agent_types = [BasicAgent, RandomAgent]
+    gamerunners = []
 
     for _ in range(args.num_games):
         agents = [agent() for agent in agent_types]
         game = Game(goal_size=10)
         #game = rig_game(game, 0)
-        game = GameRunner(game, agents, verbose=False)
+        game = GameRunner(game, agents, verbose=True)
         winner = game.play()
-        agent_scores[winner] += 1
+        gamerunners.append(game)
     
+    agent_scores = [0, 0]
+    agent_move_times = [[], []]
+    agent_longest_move_time = [0, 0]
+    for game in gamerunners:
+        agent_scores[game.game.winner] += 1
+        longest = game.get_longest_move_times()
+        for agent_id in range(len(agent_types)):
+            agent_move_times[agent_id] += game.agent_move_times[agent_id]
+            if longest[agent_id] > agent_longest_move_time[agent_id]:
+                agent_longest_move_time[agent_id] = longest[agent_id]
+
     print('Scores:')
     for i, agent_type, agent_score in zip(range(len(agent_types)), agent_types, agent_scores):
         agent = agent_type()
         print('  {} - {}: {}'.format(i, agent.name, agent_score))
+        print('    avg move time: {}'.format(sum(agent_move_times[i])/len(agent_move_times[i])))
+        print('    longest move time: {}'.format(agent_longest_move_time[i]))
 
 if __name__=='__main__':
     main()
--- src/gamerunner.py
+++ src/gamerunner.py
@@ -1,23 +1,40 @@
 from hiddengame import HiddenGame
+import time
 
 class GameRunner:
+    '''
+        Class for running and collecting information about a Game
+    '''
     def __init__(self, game, agents, verbose=True):
         if game.num_players != len(agents):
             raise RuntimeError("Wrong number of agents!")
         self.game = game
         self.agents = agents
         self.verbose = verbose
+        self.agent_move_times = [[] for _ in agents]
     
     def v_print(self, string):
         if self.verbose:
             print(string)
+
+    def get_avg_move_times(self):
+        return [sum(times)/len(times) for time in self.agent_move_times]
+
+    def get_longest_move_times(self):
+        return [max(times) for times in self.agent_move_times]
 
     def play(self):
         while True:
             # get the current agent, and ask it what move it wants to do
             agent = self.agents[self.game.current_player]
             hg = HiddenGame(self.game)
+
+            start_time = time.perf_counter()
+            # Do the move
             move = agent.get_move(hg)
+            # record how long it took
+            self.agent_move_times[self.game.current_player].append(time.perf_counter() - start_time)
+
             self.v_print("Player {} => {}".format(self.game.current_player, move.repr(hg)))
             self.v_print("=========================================================")
             # now actually do the move
